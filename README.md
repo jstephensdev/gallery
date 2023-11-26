7a70edad7b306722c91f78e6499b0246e7ad0795
“Jessica <jessica.stephens.dev@gmail.com>
2023-11-26 13:28:19 -0600

    initial laravel start

---
+++ README.md
@@ -0,0 +1,7 @@
+## Steps to creating project:
+1. php artisan make:model Image --migration 
+2. php artisan migrate
+3. 
+
+## Resources
+1. https://pqina.nl/filepond/docs/
---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+
+     public function show(){
+        return ;
+    }
+
+     public function store(){
+        return ;
+    }
+}
